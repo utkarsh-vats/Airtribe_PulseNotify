740364c484a9fcfcb8fa1b72e92dab444e2cc781
utkarsh-vats <utkarsh.vats.2k@gmail.com>
2026-05-18 19:57:19 +0530

    config: add LOGGING to base.py with verbose formatter and console handler

--- pulsenotify/settings/base.py
+++ pulsenotify/settings/base.py
@@ -106,6 +106,34 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'pulse': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
