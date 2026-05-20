53edb9a97c4580938fa8ca31fcc3d926d921bdb8
utkarsh-vats <utkarsh.vats.2k@gmail.com>
2026-05-21 03:18:21 +0530

    feat: add CORS headers for frontend integration

--- pulsenotify/settings/base.py
+++ pulsenotify/settings/base.py
@@ -18,12 +18,14 @@ INSTALLED_APPS = [
     # THIRD PARTY APPS
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
 
     # LOCAL APPS
     'pulse',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,6 +148,10 @@ CACHES = {
         'LOCATION': CELERY_BROKER_URL
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 TRAVELPAYOUTS_API_TOKEN = os.environ.get("TRAVELPAYOUTS_API_TOKEN")
 
--- pulsenotify/settings/production.py
+++ pulsenotify/settings/production.py
@@ -12,4 +12,10 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 CSRF_TRUSTED_ORIGINS = ['https://api.pulsenotify.obtuse.in']
 
+CORS_ALLOWED_ORIGINS = [
+    'https://localhost:3000',
+    'https://pulsenotify.obtuse.in',
+    'https://pulsenotify.vercel.app',
+]
+
 STATIC_ROOT = BASE_DIR / 'static'
